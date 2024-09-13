bc8af39dacb3cdb58eecac89faed413d622e0f40
Edward M <emarecos@bu.edu>
2024-09-13 14:34:41 -0400

    Update MainActivity.kt

--- week2/QuoteoftheDayApp/app/src/main/java/com/example/quoteofthedayapp/MainActivity.kt
+++ week2/QuoteoftheDayApp/app/src/main/java/com/example/quoteofthedayapp/MainActivity.kt
@@ -4,19 +4,26 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.contentColorFor
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -29,10 +36,25 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             QuoteOfTheDayAppTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Quote(
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Scaffold(
+                    modifier = Modifier
+                        .fillMaxSize()
+//                        .background(Color(0xFFd5f5e3)),
+//                        .background((Color.Black))
+                ) { innerPadding ->
+                    Box (
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .padding(innerPadding)
+                            .background(Color.Black)
+                    ) {
+                        Quote(
+                            modifier = Modifier
+                                .padding(innerPadding)
+                                .fillMaxSize()
+                                .background(Color.Black)
+                        )
+                    }
                 }
             }
         }
@@ -46,24 +68,34 @@ fun Quote(modifier: Modifier = Modifier) {
     val currentQuote = remember { mutableStateOf(quotes[randomIndex]) }
 
     Column(
-        modifier = Modifier.fillMaxSize(),
+        modifier = Modifier
+            .fillMaxSize(),
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally,
     ) {
         // display the quote
         Text(
             text = currentQuote.value,
-            modifier = modifier
+            modifier = Modifier
                 .fillMaxWidth()
                 .padding(16.dp),
-            textAlign = TextAlign.Center
+            textAlign = TextAlign.Center,
+            color = Color.White
         )
 
         // add button to change the quoter
-        Button(onClick = {
-            val newRandomIndex = Random.nextInt(quotes.size)
-            currentQuote.value = quotes[newRandomIndex]
-        }) {
+        Button(
+            modifier = Modifier
+                .clip(RoundedCornerShape(12.dp)),
+            colors = ButtonDefaults.buttonColors(
+//                backgroundColor = Color(0xFF333333),
+//                contentColorFor()
+            ),
+            onClick = {
+                val newRandomIndex = Random.nextInt(quotes.size)
+                currentQuote.value = quotes[newRandomIndex]
+            }
+        ) {
             Text(text = "Show Another Quote")
         }
     }
@@ -104,6 +136,23 @@ val quotes = listOf(
 @Composable
 fun QuotePreview() {
     QuoteOfTheDayAppTheme {
-        Quote()
+        Scaffold(
+            modifier = Modifier
+                .fillMaxSize()
+        ) { innerPadding ->
+            Box (
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(innerPadding)
+                    .background(Color(0xFFd5f5e3))
+            ) {
+                Quote(
+                    modifier = Modifier
+                        .padding(innerPadding)
+                        .fillMaxSize()
+                        .background(Color.Black)
+                )
+            }
+        }
     }
 }
